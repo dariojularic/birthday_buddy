9b2ed55056602b0180cfa2cf7bc54cf94e814e08
Dario Jularic <dariojularic0@gmail.com>
2024-09-05 08:16:51 +0200

    Add style

--- src/Buddy.jsx
+++ src/Buddy.jsx
@@ -5,14 +5,14 @@ function Buddy(props) {
   console.log(props)
   const {name, age, image} = props;
   return (
-    <>
+    <div className="birthday">
       <img className="profile-image" src={image} alt="user image" />
       <div className="user-info">
         <h3 className="user-name"> {name} </h3>
         <p className="user-age"> {age} </p>
       </div>
       <button> Delete </button>
-    </>
+    </div>
   )
 }
 
