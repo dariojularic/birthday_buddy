538bb9ae585b77df3fafc78a56f723362058a5bf
Dario Jularic <dariojularic0@gmail.com>
2024-09-05 07:52:42 +0200

    Create Buddy component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Buddy from "./Buddy";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- 
+      <h1> Birthdays Today </h1>
+      
     </>
   )
 }
---
+++ src/Buddy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Buddy(props) {
+  console.log(props)
+  return (
+    <>
+      <img src="" alt="" />
+      <div>
+        <h3></h3>
+        <p></p>
+      </div>
+      <button> Delete </button>
+    </>
+  )
+}
+
+export default Buddy;
