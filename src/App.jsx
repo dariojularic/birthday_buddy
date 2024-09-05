ead57d87b2748bae58df4401b415ba243fda029d
Dario Jularic <dariojularic0@gmail.com>
2024-09-05 08:01:27 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Buddy from "./Buddy";
+import birthdays from './data';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <h1> Birthdays Today </h1>
-      
+      {birthdays.map(birthday =>  {
+        return (
+          <Buddy key={birthday.id} {...birthday}/>
+        )
+      })}
     </>
   )
 }
