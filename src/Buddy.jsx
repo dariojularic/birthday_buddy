379fc2577d687b2d624e9986b380f15798c4788d
Dario Jularic <dariojularic0@gmail.com>
2024-09-05 08:12:48 +0200

    Add style

--- src/Buddy.jsx
+++ src/Buddy.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Buddy.css"
 
 function Buddy(props) {
   console.log(props)
   const {name, age, image} = props;
   return (
     <>
-      <img src={image} alt="user image" />
-      <div>
-        <h3> {name} </h3>
-        <p> {age} </p>
+      <img className="profile-image" src={image} alt="user image" />
+      <div className="user-info">
+        <h3 className="user-name"> {name} </h3>
+        <p className="user-age"> {age} </p>
       </div>
       <button> Delete </button>
     </>
