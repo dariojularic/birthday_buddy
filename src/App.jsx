19471f172c75cc19909613c9e94b049b42f67b18
Dario Jularic <dariojularic0@gmail.com>
2024-09-07 11:26:30 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import Buddy from "./Buddy";
 import data from './data';
-
 
 function App() {
   const [birthdays, setBirthdays] = useState(data)
--- src/Buddy.jsx
+++ src/Buddy.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./Buddy.css"
 
 function Buddy(props) {
